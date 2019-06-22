c646c3c6d027d117459148dd645b9f8261bb4c4a
Nathaly Mesquita <nnathalygm@gmail.com>
2019-06-22 13:52:32 -0300

    Add Desafio 09

--- Desafio_09.py
+++ Desafio_09.py
@@ -2,6 +2,6 @@
 largura = float(input('Insira a largura do parede: '))
 altura = float(input('Insira a Altura do parede: '))
 area = largura * altura
-l_tinta = area / 2
 print('Sua parede é de {} X {} metros quadrados, e a sua área é {} m^2 '.format(largura, altura, area))
+l_tinta = area / 2
 print('E para pintar essa parede, você precisará de {} litros de tinta por metro quadrado. '.format(l_tinta))
